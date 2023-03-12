62f20b5532984a89c352753a23accca63db3e214
Nikitin Vladimir <vreservoirv@rambler.ru>
2023-03-12 12:33:56 +0300

    CatCasCar - ArticleContentProvider

---
+++ src/ArticleContentProviderBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CatCasCarSkillboxSymfony\ArticleContentProviderBundle;
+
+
+use CatCasCarSkillboxSymfony\ArticleContentProviderBundle\DependencyInjection\ArticleContentProviderExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArticleContentProviderBundle extends Bundle
+{
+    public function getContainerExtension()
+    {
+        if ($this->extension === null) {
+            $this->extension = new ArticleContentProviderExtension();
+        }
+
+        return $this->extension;
+    }
+}
